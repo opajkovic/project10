3fbaeb43fa9657e161093ad09416d1b91c50db33
opajkovic <oljaolja170@gmail.com>
2023-06-18 22:30:58 +0200

    using custom hook without parameters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback} from 'react';
+import React, { useEffect, useState} from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -7,18 +7,19 @@ import useHttp from './hooks/use-http';
 function App() {
   const [tasks, setTasks] = useState([]);
   
-  const transformTasks = useCallback((tasksObj) => {
-    const loadedTasks = [];
-    for(const taskKey in tasksObj) {
-      loadedTasks.push({id:taskKey, text:tasksObj[taskKey].text});
-    }
-    setTasks(loadedTasks);
-  },[]);
 
-  const {isLoading, error, sendRequest:fetchTasks} = useHttp(transformTasks);
+  const {isLoading, error, sendRequest:fetchTasks} = useHttp();
 
   useEffect(() => {
-    fetchTasks({url:'https://react-app-olja-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'});
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+      for(const taskKey in tasksObj) {
+        loadedTasks.push({id:taskKey, text:tasksObj[taskKey].text});
+      }
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks({url:'https://react-app-olja-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'},transformTasks);
   }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
