d794bbca06af7dae7ae7334e659bdcc9dee137fd
opajkovic <oljaolja170@gmail.com>
2023-06-18 22:22:44 +0200

    using useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback} from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -7,20 +7,19 @@ import useHttp from './hooks/use-http';
 function App() {
   const [tasks, setTasks] = useState([]);
   
-  const transformTasks = (tasksObj) => {
+  const transformTasks = useCallback((tasksObj) => {
     const loadedTasks = [];
     for(const taskKey in tasksObj) {
       loadedTasks.push({id:taskKey, text:tasksObj[taskKey].text});
     }
     setTasks(loadedTasks);
-  }
+  },[]);
 
-  const {isLoading, error, sendRequest:fetchTasks} = useHttp({url:'https://react-app-olja-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'}, transformTasks);
+  const {isLoading, error, sendRequest:fetchTasks} = useHttp(transformTasks);
 
   useEffect(() => {
-    fetchTasks();
-    // eslint-disable-next-line
-  }, []);
+    fetchTasks({url:'https://react-app-olja-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'});
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
